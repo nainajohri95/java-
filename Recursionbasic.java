1791b6a5ae07a55f33a6ed207617df9e6794edeb
Naina Johri <93525285+nainajohri95@users.noreply.github.com>
2022-08-30 16:06:14 +0530

    Add files via upload

---
+++ Recursionbasic.java
@@ -0,0 +1,16 @@
+public class Recursionbasic {
+    public static void printincrease(int n){
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+
+        printincrease(n-1);
+        System.out.print(n+" ");
+    } 
+
+    public static void main(String[] args) {
+        int n = 10;
+        printincrease(n);
+    }
+}
