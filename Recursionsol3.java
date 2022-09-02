a283bd241586ee89ac2fba79292e7da6f164c67c
Naina Johri <93525285+nainajohri95@users.noreply.github.com>
2022-09-02 22:46:56 +0530

    Update Recursionsol3.java

--- Recursionsol3.java
+++ Recursionsol3.java
@@ -1,3 +1,5 @@
+// Given n frieds, each one can remain single or can be paired up with some other friend. Each friend can be paired only once.
+// Find out the total number of ways in which friends can remain single or can be paired up
 public class Recursionsol3 {
     public static void removeduplicate(int idx, String str, StringBuilder newstr, boolean map){
             if(idx == str.length()){
